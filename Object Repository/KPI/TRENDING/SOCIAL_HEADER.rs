e25e8365a67ac4bb189f9a0e9228661b7fb20bf6
jmanzanares <jmanzanares@YSPC-STRATAPPQA.YouSource.Forest>
2018-07-09 16:51:54 +0800

    added header

---
+++ Object Repository/KPI/TRENDING/SOCIAL_HEADER.rs
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>SOCIAL_HEADER</name>
+   <tag></tag>
+   <elementGuidId>39fd104a-207a-4c67-ba36-4d6b57c47060</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h6[contains(.,'social')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h6</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>font-weight-regular align-middle text-primary-light p-2 m-0</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+               social
+            </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[@class=&quot;ng-tns-c0-0&quot;]/main[@class=&quot;ng-tns-c0-0 ng-trigger ng-trigger-fadeAnimation&quot;]/app-app-root[@class=&quot;ng-tns-c2-1 ng-star-inserted&quot;]/div[@class=&quot;ng-tns-c2-1&quot;]/div[@class=&quot;ng-tns-c2-1&quot;]/main[@class=&quot;ng-tns-c2-1 ng-trigger ng-trigger-fadeAnimation&quot;]/app-kpi[@class=&quot;ng-tns-c26-3 ng-star-inserted&quot;]/main[@class=&quot;ng-tns-c26-3 ng-trigger ng-trigger-fadeAnimation ng-star-inserted&quot;]/app-kpi-trending[@class=&quot;ng-star-inserted&quot;]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;row board-wrapper justify-content-md-end&quot;]/div[@class=&quot;col-md-7 col-lg-9 col-xl-9 pl-md-2 pt-md-2 pl-lg-2 py-lg-2 pr-lg-2 bg-primary-bg&quot;]/app-trending[1]/div[@class=&quot;row h-100&quot;]/div[@class=&quot;d-flex col-md-12 col-lg-7 pl-md-3 pl-lg-2 pr-md-1 pr-lg-2&quot;]/div[@class=&quot;container-fluid wrapper-lg&quot;]/div[@class=&quot;row trending-feed-social&quot;]/div[@class=&quot;social col-md-6 col-lg-6 bg-white px-0 pb-1&quot;]/div[@class=&quot;header border-bottom&quot;]/h6[@class=&quot;font-weight-regular align-middle text-primary-light p-2 m-0&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
